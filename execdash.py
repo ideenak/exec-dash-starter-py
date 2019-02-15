03952192fd14a291003bc8e7c49ee7bbe194c23d
ideenak <45948963+ideenak@users.noreply.github.com>
2019-02-15 14:38:06 -0500

    Update execdash.py

--- execdash.py
+++ execdash.py
@@ -37,4 +37,46 @@ month = mreturn(monthalpha)
 csvreader = pd.read_csv("data/" + fname)
 productlist = []
 numprods = 0
-numprods2 = 0
+numprodssec = 0
+
+for n in csvreader['product']:
+    if n not in productlist:
+        numprods = numprods + 1
+        productlist.append(n)
+
+price = csvreader.groupby(csvreader['product']).sum()
+
+price = price.sort_values(by=['sales price'], ascending = False)
+#shoutout to @hiepnguyen034, learned how to do this from him
+
+totalprice = price['sales price'].sum()
+#totalprice = price.sort_values(by=[totalprice], ascending = False)
+
+#allows for the determination of top selling products, will be used later
+
+print(" ")
+
+print("----------------------------------------")
+print(" ")
+
+print("MONTH: " + month + " " + yentry)
+print(" ")
+
+print("----------------------------------------")
+print(" ")
+
+print("CALCULATING...")
+print(" ")
+
+print("----------------------------------------")
+print(" ")
+
+print("TOTAL MONTHLY SALES: $" + str("{0:,.2f}".format(totalprice)))
+print(" ")
+
+print("----------------------------------------")
+print("TOP SELLING PRODUCTS")
+
+#figure out how to find rankings of prices (iloc?)
+#figure out if sales prices need to still be sorted
+#find out how to align prices after
