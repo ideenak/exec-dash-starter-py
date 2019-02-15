796a01215ee4580e444c80b2d8318600a085b35d
ideenak <45948963+ideenak@users.noreply.github.com>
2019-02-15 13:39:23 -0500

    Update execdash.py

--- execdash.py
+++ execdash.py
@@ -13,3 +13,12 @@ def mreturn(monthalpha):
 #this function allows a month to be selected from the dictionary after the user enters a number for the month later in the program
     calendar = {'01':'January','02':'February','03':'March','04':'April', '05':'May','06':'June','07':'July','08':'August','09':'September','10':'October','11':'November','12':'December'}
     return calendar[monthalpha]
+
+
+while True:
+#this chunk of code allows the user to enter a month and year instead of having to know the exact filenames and naming conventions
+
+    monthalpha = input("Using the format MM (for example: January is 01), please enter the month for your sales data of choice:      ")
+    yentry = input("Using the format YYYY, please enter the year for your sales data of choice:      ")
+
+    fname = "sales-" + yentry + monthalpha + ".csv"
