85c944049a0f25ddf14deb5216229b4cf829492f
ideenak <45948963+ideenak@users.noreply.github.com>
2019-02-15 13:23:15 -0500

    Create execdash.py

---
+++ execdash.py
@@ -0,0 +1,10 @@
+import operator
+import matplotlib.pyplot as mplp
+import pandas as pd 
+import os
+
+print("Thank you for using the Executive Dashboard Helper Tool")
+print("----------------------------------------")
+print("Please follow the instructions closely in order to enjoy an optimal experience")
+print("Furthermore, ensure that your monthly sales files are organized in the 'Data' folder")
+print("----------------------------------------")
