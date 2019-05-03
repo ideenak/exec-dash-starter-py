dd92f582fdb674d5cde659799918172afee71fee
ideenak <45948963+ideenak@users.noreply.github.com>
2019-05-03 17:43:12 -0400

    Update execdash.py

--- execdash.py
+++ execdash.py
@@ -25,6 +25,8 @@ def mreturn(monthalpha):
         calendar = {'01':'January','02':'February','03':'March','04':'April', '05':'May','06':'June','07':'July','08':'August','09':'September','10':'October','11':'November','12':'December'}
         return calendar[monthalpha]    
 
+    
+#allowing pytest to occur via line below to separate functions from main
         
 if __name__ == "__main__":
 
